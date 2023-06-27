d15dff86a8892e620b44b56973ae6e6672d2e594
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2023-06-27 14:40:31 +0200

    install react-router-dom, import BrowserRouter, Route, Routes, create Routes in App, have all 3 pages home, user-details and confirmation working, create components for loading each page Home, Form, Confirmation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Home from './Home.jsx'
+import Form from './Form.jsx'
+import Confirmation from './Confirmation.jsx'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -7,6 +11,13 @@ function App() {
   return (
     <>
 
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/user-details" element={<Form />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
       {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
