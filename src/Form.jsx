218c35151570499bd6a52dd4f9904818651c4923
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2023-06-28 14:45:41 +0200

    add instructions to user on Form, have the 2 recipes selected displayed in user-details page, add a confirm button that at the moment only redirects to confirmation page

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom';
+
 function Form(props) {
     console.log('props', props.selectedRecipes)
+    const recipeNamesElement = props.selectedRecipes.map((recipe, index) => (< p key={index} > {recipe.title}</p >))
     return (
         <>
-            Form
+            (Form)
+
+            <div className="selected-recipes-container">
+                <h2>You have selected the following recipes:</h2>
+                {recipeNamesElement}
+                <h2>Please fill in the form to proceed:</h2>
+                <p>name</p>
+                <p>email</p>
+
+                <Link to="/confirmation" style={{ textDecoration: 'none' }}>
+                    <button>CONFIRM</button>
+                </Link>
+            </div>
 
         </>
     )
