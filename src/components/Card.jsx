3b81c41f70493d570b0cda4bbbd276bc5f66b792
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2023-08-10 17:16:45 +0200

    add background color to page for readability, add style to cards, adapt responsiveness for new card design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -19,9 +19,11 @@ function Card(props) {
         className={`card ${props.isSelected && 'selected'}`}
         onClick={() => props.handleSelectRecipe(props.recipeInfo, props.id)}>
         <img src={props.img} className="card-image" />
-        <h4 className="title blue">{props.title}</h4>
-        <p className="subtitle">{props.subtitle}</p>
-        <div className="tags-container">{tagsElements}</div>
+        <div className="card-text">
+          <h4 className="title blue">{props.title}</h4>
+          <p className="subtitle">{props.subtitle}</p>
+          <div className="tags-container">{tagsElements}</div>
+        </div>
       </div>
 
     </>
